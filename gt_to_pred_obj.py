dca89dafa4744d02c130399e96dc86165ce7d42a
sunx <1059363093@qq.com>
2020-03-10 16:12:24 +0800

    ..

--- gt_to_pred_obj.py
+++ gt_to_pred_obj.py
@@ -3,19 +3,34 @@ import json
 from tqdm import tqdm
 
 
-def to_pred(gt, pred_root):
+def to_pred(gt, pred_root, data_root):
     print('Generating prediction files with gt ...')
+    vid2pid = get_vid2pid(data_root)
     out = {}
     for vid in tqdm(gt):
         insts = gt[vid]
         for inst in insts:
             inst['score'] = 1.0
-            inst['start_fid'] = min(inst['trajectory'].keys())
-            inst['end_fid'] = '%06d' % int(max(inst['trajectory'].keys())+1)
-        out[vid] = insts
+            inst['start_fid'] = int(min(inst['trajectory'].keys()))
+            inst['end_fid'] = int(max(inst['trajectory'].keys()))+1
+
+            traj = inst['trajectory']
+            traj_new = {}
+            for fid in traj:
+                traj_new['%06d' % int(fid)] = traj[fid]
+            inst['trajectory'] = traj_new
+
+        out['%s/%s' % (vid2pid[vid], vid)] = insts
     out = {'version': 'VERSION 1.0', 'results': out}
     with open(pred_root, 'w') as f:
         json.dump(out, f)
+
+def get_vid2pid(data_root):
+    vid2pid = {}
+    for pid in os.listdir(data_root):
+        for vid in os.listdir(os.path.join(data_root, pid)):
+            vid2pid[vid] = pid
+    return vid2pid
 
 
 dataset = 'vidor_hoid_mini'
@@ -24,6 +39,7 @@ split = 'val'
 
 gt_path = 'dataset/%s_%s_%s_gt.json' % (dataset, task, split)
 pred_path = 'output/%s/%s_%s_%s_pred.json' % (task, dataset, task, split)
+data_root = 'data/%s/vidor-ilsvrc/Annotations/VID/%s' % (dataset, split)
 
 print('Loading %s' % gt_path)
 with open(gt_path) as f:
