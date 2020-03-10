6b7f68283cbdee5ef76672d720dd35e1fbc5000a
sunx <1059363093@qq.com>
2020-03-10 15:35:57 +0800

    obj_gt to pred

---
+++ gt_to_pred_obj.py
@@ -0,0 +1,32 @@
+import os
+import json
+from tqdm import tqdm
+
+
+def to_pred(gt, pred_root):
+    print('Generating prediction files with gt ...')
+    out = {}
+    for vid in tqdm(gt):
+        insts = gt[vid]
+        for inst in insts:
+            inst['score'] = 1.0
+            inst['start_fid'] = min(inst['trajectory'].keys())
+            inst['end_fid'] = '%06d' % int(max(inst['trajectory'].keys())+1)
+        out[vid] = insts
+
+    with open(pred_root, 'w') as f:
+        json.dump(out, f)
+
+
+dataset = 'vidor_hoid_mini'
+task = 'obj'
+split = 'val'
+
+gt_path = 'dataset/%s_%s_%s_gt.json' % (dataset, task, split)
+pred_path = 'output/%s/%s_%s_%s_pred.json' % (task, dataset, task, split)
+
+print('Loading %s' % gt_path)
+with open(gt_path) as f:
+    gt = json.load(f)
+
+to_pred(gt, pred_path)
